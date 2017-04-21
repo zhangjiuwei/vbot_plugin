fd02457cd048fbbed85924066a4a8b1040dac184
zhangwei26 <zhangwei26@baidu.com>
2017-04-21 23:46:16 +0800

    modify

---
+++ webroot/restart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cat << EOF
+Content-Type: text/html
+
+<html>
+<head>
+<title>cgi shell scripting example\</title>
+</head>
+<body>
+<h1>
+
+
+`supervisorctl restart vbot`
+
+</h1>
+</body>
+</html>
+EOF
+
